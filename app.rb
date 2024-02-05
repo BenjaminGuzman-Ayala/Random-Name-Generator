0fd3eab5700af98733011b6d771a209c616df272
Benjamin Guzman-Ayala <150963859+BenjaminGuzman-Ayala@users.noreply.github.com>
2024-02-05 05:10:57 +0000

    website

--- app.rb
+++ app.rb
@@ -1,9 +1,22 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
 end
+
+get("/generator") do
+  raw_data = HTTP.get("https://api.namefake.com/random/random")
+  parsed_data = JSON.parse(raw_data)
+  @generator = parsed_data.fetch("name")
+  erb(:name_generator)
+end
+
+#get("/meowfacts") do
+  #raw_data = HTTP.get("https://meowfacts.herokuapp.com/")
+  #@fact = JSON.parse(raw_data).fetch("data").first
+
+  #erb(:cat_fact)
+#end
